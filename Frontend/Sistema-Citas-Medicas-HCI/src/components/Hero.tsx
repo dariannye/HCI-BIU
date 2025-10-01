f2a2390bc89fcd047048159fafea2ef5012a920e
dariannye <dariannyn19@gmail.com>
2025-09-30 21:49:34 -0400

    deteccion de usuario para el boton "Mis consultas"

--- Frontend/Sistema-Citas-Medicas-HCI/src/components/Hero.tsx
+++ Frontend/Sistema-Citas-Medicas-HCI/src/components/Hero.tsx
@@ -1,6 +1,25 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 export default function Hero() {
+  const navigate = useNavigate();
+  const authContext = useContext(AuthContext);
+
+  if (!authContext) throw new Error("AuthContext must be used within AuthProvider");
+
+  const { user } = authContext;
+
+  const handleClick = () => {
+    if (user) {
+      // Redirigir según rol
+      if (user.role === "patient") {
+        navigate("/dashboard"); // dashboard del paciente
+      }
+    } else {
+      navigate("/login"); // usuario no logeado
+    }
+  };
   return (
     <section className="bg-gradient-to-r from-blue-50 to-blue-100 py-20 text-center">
       <h2 className="text-4xl font-extrabold text-blue-700">
@@ -10,9 +29,12 @@ export default function Hero() {
         Agenda tus citas médicas de forma rápida, sencilla y segura.
       </p>
       <div className="mt-6">
-        <Link to="/login" className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-700">
+         <button
+          onClick={handleClick}
+          className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-700"
+        >
           Mis consultas
-        </Link>
+        </button>
       </div>
     </section>
   );
